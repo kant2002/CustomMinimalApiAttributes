4349ec1aa698a3578c09b6f24978c8218416371d
Andrii Kurdiumov <kant2002@gmail.com>
2024-06-13 18:30:46 +0500

    Add project files.

---
+++ CustomMinimalApiAttributes/Program.cs
@@ -0,0 +1,56 @@
+using System.Reflection.Metadata;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", (CustomType type) =>
+{
+    var forecast = Enumerable.Range(1, type.X).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+class CustomType
+{
+    public int X { get; set; } = 5;
+    public static ValueTask<CustomType> BindAsync(HttpContext context, ParameterInfo parameter)
+    {
+        return new ValueTask<CustomType>(new CustomType());
+    }
+}
